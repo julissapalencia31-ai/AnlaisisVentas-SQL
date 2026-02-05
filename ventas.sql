d78b0df9d0360da18b36c828d90ecbd9c238a384
Julissa Arreola <julissapalencia31@aragon.unam.mx>
2026-02-05 13:22:18 -0600

    Update ventas.sql
    correcion de cracteres

--- ventas.sql
+++ ventas.sql
@@ -10,8 +10,8 @@ CREATE TABLE ventas(
 INSERT TO ventas VALUES
 (1, '2025-01-25', 'Labial', 'Boca', 3, 350.00),
 (2, '2025-04-25', 'rimel', 'Ojos', 2, 3425.23),
-(3, '2025-12-14', 'Maquillaje' 'Rostro', 1, 750.00)
-(4, '2025,10,04', 'serum de pestañas', 'Ojos', 15, 1648.00),
+(3, '2025-12-14', 'Maquillaje', 'Rostro', 1, 750.00)
+(4, '2025-10-04', 'serum de pestañas', 'Ojos', 15, 1648.00),
 (5, '2025-09-27', 'Rubor', 'Rostro', 58, 125.00); 
 
 SELECT * FROM ventas;
